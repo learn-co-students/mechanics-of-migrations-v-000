3c77d7d301e8d79b5cd91330b2861da5cd53d54e
bryanhou1 <Hou@Bryans-MacBook-Pro.local>
2017-04-10 22:31:51 -0400

    5 tests passing, with strange SQL occurence

--- artist.rb
+++ artist.rb
@@ -1,7 +1,33 @@
 class Artist < ActiveRecord::Base
+	  sql = <<-SQL
+		      CREATE TABLE IF NOT EXISTS artists (
+		        id INTEGER PRIMARY KEY,
+		        name TEXT,
+		        genre TEXT,
+		        age INTEGER,
+		        hometown TEXT
+		      );
+		    	SQL
+		ActiveRecord::Base.connection.execute(sql)
 
   def self.create_table
-    # create table using SQL here
+  ## ??? This does not work despite appearing identical.
+  # 	sql = <<-SQL
+  # 	CREATE TABLE IF NOT EXISTS songs (
+		# 	id INTEGER PRIMARY KEY,
+		# 	title TEXT,
+		# 	length INTEGER
+		# 	);
+		# SQL 
+
+		sql = <<-SQL
+		  CREATE TABLE IF NOT EXISTS songs (
+		  id INTEGER PRIMARY KEY,
+		  title TEXT,
+		  length INTEGER
+		  );
+		SQL
+		ActiveRecord::Base.connection.execute(sql)
   end
 
 end
