55b1e5bfb72c664885550c5acdbf850a502b080f
github username <github email address>
2017-10-02 15:31:08 -0500

    Done.

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -5,19 +5,6 @@ class CreateArtists < ActiveRecord::Migration[5.0]
   #    :database => "db/artists.sqlite"
   #)
 
-  def create_table
-    sql = <<-SQL
-      CREATE TABLE IF NOT EXISTS artists (
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        genre TEXT,
-        age INTEGER,
-        hometown TEXT
-      )
-      SQL
-    ActiveRecord::Base.connection.execute(sql)
-  end
-
   def change
     create_table :artists do |t|
       t.string :name
