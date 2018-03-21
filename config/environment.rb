9316f990eb56cb66a9efabe7ee5b09af34c53fa4
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-03-21 22:48:49 +0000

    created create artists file

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,20 @@ require 'bundler/setup'
 Bundler.require
 
 
-# put the code to connect to the database here
+connection = ActiveRecord::Base.establish_connection(
+  :adapted => "sqlite3",
+  :database => "db/artists.sqlite"
+  )
+  
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS artists (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  genre TEXT,
+  age INTEGER,
+  hometown TEXT
+  )
+SQL
 
 
 require_relative "../artist.rb"
