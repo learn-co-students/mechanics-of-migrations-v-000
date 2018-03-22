c467a6a646fda27f3c364601ffbbde239f7264c8
Sam. O <samrey2018@gmail.com>
2018-03-22 21:42:11 +0000

    Done.

--- artist.rb
+++ artist.rb
@@ -1,4 +1,4 @@
-#testing 
+#testing
  class Artist < ActiveRecord::Base
 
  end
--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -3,14 +3,8 @@
              create_table :artists do |t|
                  t.string :name
                   t.string :genre
-
                   t.string :age
-
                   t.string :hometown
              end
-
          end
-
  end
-
-#testing 2
