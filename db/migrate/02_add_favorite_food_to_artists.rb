409abdbd0c226fff77527f34194164270797e84d
Sam. O <samrey2018@gmail.com>
2018-03-23 18:05:25 +0000

    Done.

--- db/migrate/02_add_favorite_food_to_artists.rb
+++ db/migrate/02_add_favorite_food_to_artists.rb
@@ -1,7 +1,8 @@
-class AddFavoriteFoodToArtists < ActiveRecord::Migration
+
+    class AddFavoriteFoodToArtists < ActiveRecord::Migration[4.2]
+
     def change
         add_column :artists, :favorite_food, :string
-
     end
 
 end
