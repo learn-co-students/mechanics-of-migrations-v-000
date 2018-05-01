f5045f3473818b9163989fc821eac005077044f2
Brandon Brasson <rocksong622@gmail.com>
2018-05-01 08:45:05 +0000

    Done.

--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -1,6 +1,6 @@
 require_relative './spec_helper'
 
-describe "Artist" do
+puts describe "Artist" do
   let(:artist) {Artist.new(name: 'Justice', genre: 'Electronic', age: Time.now.strftime("%Y").to_i - 2003, hometown: "Paris")}
 
   it "can instantiate a new instance" do
@@ -35,7 +35,7 @@ describe "Artist" do
     expect(Artist.find_by(name: "Taylor Swift")).to eq(taytay)
   end
 
-  it "can roll back to have no favorite_food attribute for Artist" do    
+  it "can roll back to have no favorite_food attribute for Artist" do
     expect(Artist.column_names).to eq(["id", "name", "genre", "age", "hometown"])
   end
 end
