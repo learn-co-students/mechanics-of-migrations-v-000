f6fc79f57af3da1a877a6dee3e3fb0dcc9a4612a
Lorena <lorena.giddens@outlook.com>
2018-07-24 18:13:43 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,11 @@ require 'bundler/setup'
 Bundler.require
 
 
-# put the code to connect to the database here
-
+# use the establish_connection method from ActiveRecord::Base to connect to
+# artists database, which will be created in the migration via SQLite3
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
 
 require_relative "../artist.rb"
