c90cfd2b2b6349cfa664c6215baf377b038ee3bc
Brittany Tinnin <bntinnin7@gmail.com>
2018-08-03 18:10:05 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -5,11 +5,24 @@ require 'ostruct'
 require 'date'
 
 
+ActiveRecord::Base.connection.execute(sql)
+
 require 'bundler/setup'
 Bundler.require
 
+connection = ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
 
-
-
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS artists (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  genre TEXT,
+  age INTEGER,
+  hometown TEXT
+  )
+SQL
 
 require_relative "../artist.rb"
