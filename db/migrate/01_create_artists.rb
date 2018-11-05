7718fa8b7411ad13f713b07e88a2df947c49885f
github username <github email address>
2018-11-04 18:50:29 -0700

    Done.

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,10 +1,10 @@
 class CreateArtists < ActiveRecord::Migration
-  
-    def up 
-    end
 
-    def down 
-    end
+    #def up 
+    #end
+
+    #def down 
+    #end
 
     def change 
       create_table :artists do |t|
