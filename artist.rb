33ebc82a73315760f334cd4bf1d1b4c7d3eb2795
Meg Gutshall <meghan.gutshall@gmail.com>
2018-11-19 20:15:52 -0500

    Done.

--- artist.rb
+++
@@ -1,5 +0,0 @@
-class Artist < ActiveRecord::Base
-
-  
-
-end
--- config/environment.rb
+++ config/environment.rb
@@ -9,10 +9,7 @@ require 'bundler/setup'
 Bundler.require
 
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/artists.sqlite"
-)
+
 
 
 require_relative "../artist.rb"
