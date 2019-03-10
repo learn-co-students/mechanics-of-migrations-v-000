116339f83a37ef919b3b8dbe5c5867c63b73f297
sarahpaz <sarah.paz@gmail.com>
2019-03-09 20:01:40 -0500

    Done.

---
+++ db/migrate/01_create_artists.rb
@@ -0,0 +1,18 @@
+class CreateArtists < ActiveRecord::Migration
+  def up
+  
+  end
+
+  def down
+
+  end
+
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.string :age
+      t.string :hometown
+    end
+  end
+end
