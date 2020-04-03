6d0c52c55592d4905be13106b1db69b4a5f46615
Amar Chatman <amarjerome@gmail.com>
2020-04-03 23:09:59 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -18,3 +18,5 @@ ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/artists.sqlite"
 )
+
+require_relative '../artist.rb'
--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,27 +1,27 @@
 class CreateArtists < ActiveRecord::Migration[5.2]
 
-ActiveRecord::Base.establish_connection(
-    :adapter => "sqlite3",
-    :database => "db/artists.sqlite"
-  )
-  sql = <<-SQL
-  CREATE TABLE IF NOT EXISTS artists (
-  id INTEGER PRIMARY KEY,
-  name TEXT,
-  genre TEXT,
-  age INTEGER,
-  hometown TEXT
-  )
-SQL
-
-ActiveRecord::Base.connection.execute(sql)
-
-
-  def up
-  end
-
-  def down
-  end
+# ActiveRecord::Base.establish_connection(
+#     :adapter => "sqlite3",
+#     :database => "db/artists.sqlite"
+#   )
+#   sql = <<-SQL
+#   CREATE TABLE IF NOT EXISTS artists (
+#   id INTEGER PRIMARY KEY,
+#   name TEXT,
+#   genre TEXT,
+#   age INTEGER,
+#   hometown TEXT
+#   )
+# SQL
+#
+# ActiveRecord::Base.connection.execute(sql)
+#
+#
+#   def up
+#   end
+#
+#   def down
+#   end
 
   def change
     create_table :artists do |t|
@@ -32,4 +32,4 @@ ActiveRecord::Base.connection.execute(sql)
     end
   end
 end
-end  
+  
